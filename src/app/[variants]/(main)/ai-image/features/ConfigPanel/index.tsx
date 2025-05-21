1412418ecb63f9944024075e662e276377ba84dd
YuTengjing <ytj2713151713@gmail.com>
2025-05-21 15:51:23 +0800

    feat: ai image page basic layout

---
+++ src/app/[variants]/(main)/ai-image/features/ConfigPanel/index.tsx
@@ -0,0 +1,17 @@
+import { Flexbox } from 'react-layout-kit';
+
+const ConfigPanel = () => {
+  return (
+    <Flexbox
+      style={{
+        height: '100%',
+        overflowY: 'auto',
+        width: 260,
+      }}
+    >
+      配置面板
+    </Flexbox>
+  );
+};
+
+export default ConfigPanel;
